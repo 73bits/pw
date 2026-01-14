7d6bd07b362b2ac07edaca17fc340bddc4d41027
73bits <prashantkr0628@gmail.com>
2026-01-14 19:33:40 +0530

    modified model: Entry; added model: Vault

--- internal/model/entry.go
+++ internal/model/entry.go
@@ -2,7 +2,9 @@ package model
 
 type Entry struct {
 	Name     string `json:"name"`
-	Username string `json:"username"`
 	Password string `json:"password"`
-	Notes    string `json:"notes,omitempty"`
+}
+
+type Vault struct {
+	Entries []Entry `json:"entries"`
 }
