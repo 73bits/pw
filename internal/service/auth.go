079f79041e3c4d0e31a96b38482a9367e0e8f52d
73bits <prashantkr0628@gmail.com>
2026-01-14 19:43:00 +0530

    defined Encrypt and Decrypt methods; takes key and []byte data

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,10 +1,57 @@
 package service
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/pbkdf2"
+	"crypto/rand"
 	"crypto/sha256"
+	"errors"
+	"io"
 )
 
 func DeriveKey(password string, salt []byte) ([]byte, error) {
 	return pbkdf2.Key(sha256.New, password, salt, 100000, 32)
+}
+
+func Encrypt(key, plaintext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	// output = nonce || ciphertext || authTag
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func Decrypt(key, data []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	ns := gcm.NonceSize()
+	if len(data) < ns {
+		return nil, errors.New("invalid data")
+	}
+
+	nonce := data[:ns]
+	ciphertext := data[ns:]
+
+	return gcm.Open(nil, nonce, ciphertext, nil)
 }
