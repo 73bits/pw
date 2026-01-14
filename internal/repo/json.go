3038e4818add0999278de6d5465ed97eb23e2c62
73bits <prashantkr0628@gmail.com>
2026-01-14 19:41:30 +0530

    modified Save(); added Init and NewJSONRepo method

--- internal/repo/json.go
+++ internal/repo/json.go
@@ -1,6 +1,9 @@
 package repo
 
-import "os"
+import (
+	"errors"
+	"os"
+)
 
 type JSONRepo struct {
 	Path string
@@ -11,5 +14,22 @@ func (s *JSONRepo) Load() ([]byte, error) {
 }
 
 func (s *JSONRepo) Save(data []byte) error {
+	tmp := s.Path + ".tmp"
+	if err := os.WriteFile(tmp, data, 0600); err != nil {
+		return err
+	}
+	return os.Rename(tmp, s.Path)
+}
+
+func NewJSONRepo(path string) *JSONRepo {
+	return &JSONRepo{Path: path}
+}
+
+func (s *JSONRepo) Init(data []byte) error {
+	_, err := os.Stat(s.Path)
+	if err == nil {
+		return errors.New("valut already exist")
+	}
+
 	return os.WriteFile(s.Path, data, 0600)
 }
